201e778c73676b89d6518dc6bb72138e2106df90
Marjorie Roswell <mroswell@gmail.com>
2013-07-26 20:52:55 -0400

    Clear entered to-do item from the input box

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("#addtodo").click(function () {
         var todoitem = $('#todo').val();
+		$('#todo').val('');
         $('#gohere').append('<li class="item">' + todoitem + '</li>');
         $('.item').click(function () {
             $(this).remove();
